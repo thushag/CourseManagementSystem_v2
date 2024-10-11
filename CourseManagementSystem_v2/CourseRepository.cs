aedde5ecedf42694fdac17a03583de26a99027e7
UT01138 <UT01138@UNICOMTIC12>
2024-10-11 17:00:58 +0530

    Database Connection

---
+++ CourseManagementSystem_v2/CourseRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v2
+{
+    public class CourseRepository
+    {
+        private readonly string _connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=CourseManagement;Trusted_Connection=true;TrustServerCertificate=true;";
+
+
+        public void AddCourses()
+        {
+
+        }
+
+
+    }
+}
--- CourseManagementSystem_v2/Program.cs
+++ CourseManagementSystem_v2/Program.cs
@@ -10,6 +10,68 @@ namespace CourseManagementSystem_v2
     {
         static void Main(string[] args)
         {
+            CourseManager manager = new CourseManager();
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("Course Management System");
+                Console.WriteLine("1. Add a Course");
+                Console.WriteLine("2. View All Courses");
+                Console.WriteLine("3. Update a Course");
+                Console.WriteLine("4. Delete a Course");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("Choose an Option");
+                var option = Console.ReadLine();
+                while (option != null)
+                {
+                    switch (option)
+                    {
+                        case "1":
+                            Console.WriteLine("Enter Course Id");
+                            string CourseId = Console.ReadLine();
+
+                            Console.WriteLine("Enter Course Title");
+                            string Title = Console.ReadLine();
+
+                            Console.WriteLine("Enter Course Duration");
+                            string Duration = Console.ReadLine();
+
+                            Console.WriteLine("Enter Course Price");
+                            decimal Price = Console.ReadLine();
+
+                            manager.CreateCourse(CourseId, Title, Duration, Price);
+                            break;
+
+                        case "2":
+                            manager.ReadCourses();
+
+                            break;
+
+                        case "3":
+                            Console.WriteLine("Enter Course Id");
+                            string CourseId = Console.ReadLine();
+
+                            manager.DeleteCourse(CourseId);
+                            break;
+
+                        case "4":
+                            Console.WriteLine("Enter Course Id");
+                            string CourseId = Console.ReadLine();
+                            manager.DeleteCourse(CourseId);
+                            break;
+
+                        case "5":
+                            bool exit = true;
+                            break;
+
+                        default:
+                            Console.WriteLine("Choose correct option");
+
+
+                    }
+                    Console.WriteLine("Press any key to continue");
+                    Console.ReadKey();
+                }
+            }
         }
-    }
 }
