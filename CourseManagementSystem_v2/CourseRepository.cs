27b9674664300e3fe3c1f3a57921a90f64f4523d
UT01138 <UT01138@UNICOMTIC12>
2024-10-11 17:05:38 +0530

    Add Course

--- CourseManagementSystem_v2/CourseRepository.cs
+++ CourseManagementSystem_v2/CourseRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,15 @@ namespace CourseManagementSystem_v2
 
         public void AddCourses()
         {
+            try
+            {
+               using (var connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+                    var command = connection.CreateCommand();
 
+                }
+            }
         }
 
 
