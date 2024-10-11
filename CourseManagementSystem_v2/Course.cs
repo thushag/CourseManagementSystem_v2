857685d6b8a06221e641eedfddd3039644b7c740
UT01138 <UT01138@UNICOMTIC12>
2024-10-11 16:45:51 +0530

    Question 11

---
+++ CourseManagementSystem_v2/Course.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v2
+{
+    public class Course
+    {
+        public string CourseId { get; set; }
+        public string Title { get; set; }
+        public string Duration { get; set; }
+        public decimal Price { get; set; }
+
+        public Course(string courseId,string title,string duration,decimal price)
+        {
+            CourseId = courseId;
+            Title = title;
+            Duration = duration;
+            Price = price;
+        }
+            
+
+    }
+}
